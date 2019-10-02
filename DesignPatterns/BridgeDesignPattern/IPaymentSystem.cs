00976847c3ea1da70684ebeff0e5be58fe5adc8d
sachidanand suraj <suraj.thepions@gmail.com>
2019-10-02 15:22:51 +0530

    sol restructured

---
+++ DesignPatterns/BridgeDesignPattern/IPaymentSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeDesignPattern
+{
+    /// <summary>
+    /// The 'Bridge/Implementor' interface
+    /// </summary>
+    public interface IPaymentSystem
+    {
+        void ProcessPayment(string paymentSystem);
+    }
+}
