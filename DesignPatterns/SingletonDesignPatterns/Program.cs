5953073fc66c6b16589e7d565831e9f64334a825
sachidanand suraj <suraj.thepions@gmail.com>
2020-01-12 00:11:52 +0530

    Updated the folder structure
    Updated the folder structure

---
+++ DesignPatterns/SingletonDesignPatterns/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SingletonDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.Instance;
+            Singleton singleton2 = Singleton.Instance;
+            singleton1.Increment();
+            if(singleton1.Value == singleton2.Value)
+                Console.WriteLine("singleton object");
+
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Load balance 15 server requests
+
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatterns/SingletonDesignPatterns/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDesignPatterns
+{
+    class Singleton
+    {
+        private int value = 10;
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get { return Nested.instance; }
+        }
+
+        private class Nested
+        {
+            static Nested() { }
+
+            internal static readonly Singleton instance = new Singleton();
+        }
+        public void Increment()
+        {
+            value++;
+        }
+        public int Value { get { return value; } }
+    }
+}
